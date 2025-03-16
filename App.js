2e3af670d1a7e5026cc88e0ea3bdb625257e0575
juanan-93 <juanan-93@hotmail.com>
2025-03-16 16:50:22 +0100

    carpetas

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>estas dentro primera app</Text>
+      <Text>EN PROCESO ...</Text>
       <StatusBar style="auto" />
     </View>
   );
